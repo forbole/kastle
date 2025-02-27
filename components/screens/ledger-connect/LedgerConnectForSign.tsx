2fdae1e4b0ed25c7480225666de49895060122b6
dadamu <p22626262@gmail.com>
2025-02-27 18:48:00 +0800

    fix: remove console log

--- components/screens/ledger-connect/LedgerConnectForSign.tsx
+++ components/screens/ledger-connect/LedgerConnectForSign.tsx
@@ -39,7 +39,6 @@ export default function LedgerConnectForSign() {
     try {
       await connect();
     } catch (error) {
-      console.log(error);
       setTimeout(
         () =>
           navigate({
