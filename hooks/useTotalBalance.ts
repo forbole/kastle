82916a48887ee2832f63f8b69ab099f4ecfb8b45
0x7u <0xju@pm.me>
2025-03-21 12:06:22 +0800

    fix: missing decimal places

--- hooks/useTotalBalance.ts
+++ hooks/useTotalBalance.ts
@@ -1,30 +1,32 @@
 import { useTokensMetadata } from "@/hooks/useTokenMetadata.ts";
+import { applyDecimal } from "@/lib/krc20.ts";
 
 export default function useTotalBalance() {
   const { account } = useWalletManager();
   const kapsaPrice = useKaspaPrice();
   const balance = account?.balance;
+  const kaspaUsd = balance ? parseFloat(balance) * kapsaPrice.kaspaPrice : 0;
+
   const { data: tokenListResponse } = useTokenListByAddress(
     account?.address,
     5000,
   );
-  const kaspaUsd = balance ? parseFloat(balance) * kapsaPrice.kaspaPrice : 0;
 
   const balancePerTicker = tokenListResponse?.result.reduce<
-    Record<string, string>
+    Record<string, number>
   >((acc, token) => {
-    acc[token.tick] = token.balance;
+    const { toFloat } = applyDecimal(token.dec);
+    acc[token.tick] = toFloat(parseInt(token.balance, 10));
     return acc;
   }, {});
   const tickers = Object.keys(balancePerTicker ?? {});
-
   const { toPriceInUsd } = useTokensMetadata(tickers);
   const priceInUsdPerTicker = toPriceInUsd();
 
   const totalTokenUsd = Object.entries(balancePerTicker ?? {}).reduce<number>(
     (acc, [ticker, amount]) => {
       const usdPriceTicker = priceInUsdPerTicker?.[ticker] ?? 0;
-      const totalTickerUsd = parseFloat(amount) * usdPriceTicker;
+      const totalTickerUsd = amount * usdPriceTicker;
       return acc + totalTickerUsd;
     },
     0,
