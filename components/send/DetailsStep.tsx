86a0bf0f96b7f4031db955831436607874490c4b
Paul Chen <p22626262@gmail.com>
2025-06-13 16:09:02 +0800

    fix: add relative to send component

--- components/send/DetailsStep.tsx
+++ components/send/DetailsStep.tsx
@@ -300,17 +300,7 @@ export const DetailsStep = ({
   return (
     <>
       <Header title="Send" onClose={onClose} onBack={onBack} />
-      <div className="relative flex h-full flex-col gap-4">
-        <TickerSelect
-          isShown={isTokenSelectShown}
-          toggleShow={toggleTokenSelect}
-        />
-
-        <PriorityFeeSelection
-          isPriorityFeeSelectionOpen={isPriorityFeeSelectionOpen}
-          closePriorityFeeSelection={closePriorityFeeSelection}
-        />
-
+      <div className="flex h-full flex-col gap-4">
         <div className="flex items-center justify-between">
           <label className="text-base font-medium">Send to ...</label>
           <i
@@ -563,6 +553,16 @@ export const DetailsStep = ({
           </button>
         </div>
       </div>
+
+      <TickerSelect
+        isShown={isTokenSelectShown}
+        toggleShow={toggleTokenSelect}
+      />
+
+      <PriorityFeeSelection
+        isPriorityFeeSelectionOpen={isPriorityFeeSelectionOpen}
+        closePriorityFeeSelection={closePriorityFeeSelection}
+      />
     </>
   );
 };
