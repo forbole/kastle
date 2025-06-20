c749b769ad74724929b905bc336f6ec9bd5998f5
Paul Chen <p22626262@gmail.com>
2025-06-20 16:53:34 +0800

    feat: disable evm kas sending for ledger

--- components/send/token-selector/EvmKasSelectItem.tsx
+++ components/send/token-selector/EvmKasSelectItem.tsx
@@ -3,16 +3,22 @@ import kasIcon from "@/assets/images/kas-icon.svg";
 import useEvmKasBalance from "@/hooks/evm/useEvmKasBalance";
 import { useNavigate } from "react-router-dom";
 import { getChainImage } from "@/lib/layer2";
+import useWalletManager from "@/hooks/useWalletManager";
+import { twMerge } from "tailwind-merge";
 
 export default function EvmKasSelectItem({
   chainId,
 }: {
   chainId: `0x${string}`;
 }) {
+  const { wallet } = useWalletManager();
   const navigate = useNavigate();
   const { data } = useEvmKasBalance(chainId);
   const kasBalance = data?.balance ?? "0";
+
+  const isLedger = wallet?.type === "ledger";
   const onClick = () => {
+    if (isLedger) return;
     navigate(`/evm-kas/send/${chainId}`);
   };
 
@@ -24,7 +30,12 @@ export default function EvmKasSelectItem({
           className="flex items-center justify-between rounded-lg px-4 py-2 text-base font-medium text-daintree-200 hover:bg-daintree-700"
           onClick={onClick}
         >
-          <div className="flex items-center gap-2">
+          <div
+            className={twMerge(
+              "flex items-start gap-2",
+              isLedger && "opacity-40",
+            )}
+          >
             <Layer2AssetImage
               tokenImage={kasIcon}
               tokenImageSize={24}
@@ -33,7 +44,12 @@ export default function EvmKasSelectItem({
             />
             <span>KAS</span>
           </div>
-          <span>{kasBalance}</span>
+          {!isLedger && <span>{kasBalance.toLocaleString()}</span>}
+          {isLedger && (
+            <span className="rounded-full bg-[#1C333C] p-2 px-4 text-xs text-white">
+              Not supported with Ledger
+            </span>
+          )}
         </button>
       )}
     </>
