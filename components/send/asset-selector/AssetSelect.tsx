59e31c4573cedeaf1fe22553be6988297817e2f0
Paul Chen <p22626262@gmail.com>
2025-07-04 17:30:34 +0800

    feat: add custom scrollbar for asset selector

--- components/send/asset-selector/AssetSelect.tsx
+++ components/send/asset-selector/AssetSelect.tsx
@@ -23,7 +23,9 @@ export default function AssetSelect() {
   const { assets } = useErc20Assets();
 
   const filteredErc20Assets = assets.filter((asset) =>
-    hasSearchQuery ? true : asset.symbol.startsWith(searchQuery),
+    hasSearchQuery
+      ? true
+      : asset.symbol.toLowerCase().startsWith(searchQuery.toLowerCase()),
   );
   const tokens = tokenListItems
     ?.filter((token) =>
@@ -41,22 +43,24 @@ export default function AssetSelect() {
     });
 
   return (
-    <div className="h-full w-full p-4">
-      <Header title="Select Asset" onClose={() => navigate("/dashboard")} />
-      <div className="relative flex items-center pb-4">
-        <div className="pointer-events-none absolute start-0 flex items-center ps-4 leading-none text-daintree-400 peer-disabled:pointer-events-none peer-disabled:opacity-50">
-          <i className="hn hn-search text-base font-medium"></i>
-        </div>
+    <div className="flex h-full w-full flex-col">
+      <div className="px-4 pt-4">
+        <Header title="Select Asset" onClose={() => navigate("/dashboard")} />
+        <div className="relative flex items-center pb-4">
+          <div className="pointer-events-none absolute start-0 flex items-center ps-4 leading-none text-daintree-400 peer-disabled:pointer-events-none peer-disabled:opacity-50">
+            <i className="hn hn-search text-base font-medium"></i>
+          </div>
 
-        <input
-          type="text"
-          className="block w-full rounded-lg border-daintree-700 bg-[#051D26] pe-4 ps-10 text-sm text-white placeholder-daintree-400 focus:border-daintree-700 focus:ring-daintree-700 disabled:pointer-events-none disabled:opacity-50"
-          placeholder="Search token"
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
+          <input
+            type="text"
+            className="block w-full rounded-lg border-daintree-700 bg-daintree-800 py-3 pe-4 ps-10 text-sm text-white placeholder-daintree-400 focus:border-daintree-700 focus:ring-daintree-700 disabled:pointer-events-none disabled:opacity-50"
+            placeholder="Search token"
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+        </div>
       </div>
 
-      <div className="no-scrollbar flex flex-col overflow-y-scroll">
+      <div className="thin-scrollbar mb-4 mr-1 flex flex-1 flex-col overflow-y-scroll pl-4 pr-3">
         {/* KAS */}
         {isKasShown && (
           <button
