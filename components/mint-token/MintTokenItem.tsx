8be4fd91ebbcc31d207556cfa4b5cef789eae848
Paul Chen <p22626262@gmail.com>
2025-07-04 21:15:52 +0800

    fix(ui): fix mint krc20 ui

--- components/mint-token/MintTokenItem.tsx
+++ components/mint-token/MintTokenItem.tsx
@@ -4,6 +4,7 @@ import { DeployFormData } from "@/components/screens/full-pages/MintToken.tsx";
 import kasIcon from "@/assets/images/network-logos/kaspa.svg";
 import { applyDecimal } from "@/lib/krc20.ts";
 import { useTokenMetadata } from "@/hooks/kasplex/useTokenMetadata.ts";
+import { useTokenInfo } from "@/hooks/kasplex/useTokenInfo";
 
 interface MintTokenItemProps {
   token?: any;
@@ -16,6 +17,7 @@ export default function MintTokenItem({ token }: MintTokenItemProps) {
   const [imageUrl, setImageUrl] = useState(kasIcon);
   const { toFloat } = applyDecimal(token.dec);
   const balance = parseInt(token.balance, 10);
+  const { data: tokenInfo } = useTokenInfo(token.id);
 
   const onImageError = () => {
     setImageUrl(kasIcon);
@@ -27,22 +29,28 @@ export default function MintTokenItem({ token }: MintTokenItemProps) {
     }
   }, [tokenMetadata?.iconUrl]);
 
+  const isMintMode = tokenInfo?.result?.[0]?.mod === "mint";
+
   return (
-    <button
-      type="button"
-      className="flex items-center justify-between rounded-lg px-4 py-2 text-base font-medium text-daintree-200 hover:bg-daintree-700"
-      onClick={() => setValue("ticker", token.tick, { shouldValidate: true })}
-    >
-      <div className="flex items-center gap-2">
-        <img
-          alt="castle"
-          className="h-[24px] w-[24px] rounded-full"
-          src={imageUrl}
-          onError={onImageError}
-        />
-        <span>{token.tick}</span>
-      </div>
-      <span>{toFloat(balance).toLocaleString()}</span>
-    </button>
+    <>
+      {isMintMode && (
+        <button
+          type="button"
+          className="flex items-center justify-between rounded-lg px-4 py-2 text-base font-medium text-daintree-200 hover:bg-daintree-700"
+          onClick={() => setValue("ticker", token.id, { shouldValidate: true })}
+        >
+          <div className="flex items-center gap-2">
+            <img
+              alt="castle"
+              className="h-[24px] w-[24px] rounded-full"
+              src={imageUrl}
+              onError={onImageError}
+            />
+            <span>{token.id}</span>
+          </div>
+          <span>{toFloat(balance).toLocaleString()}</span>
+        </button>
+      )}
+    </>
   );
 }
