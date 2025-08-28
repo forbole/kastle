4cbaa345dac44c162b98a47145044e1a7d600242
Paul Chen <p22626262@gmail.com>
2025-08-28 18:40:40 +0800

    feat: update event sponsorship message to include flag emoji

--- components/screens/Dashboard.tsx
+++ components/screens/Dashboard.tsx
@@ -366,7 +366,7 @@ export default function Dashboard() {
             </span>
             <span className="text-xs">
               {
-                "We’re the official wallet sponsor of The Kaspa Experience Berlin :de: on Sept 13 — join us "
+                "We’re the official wallet sponsor of The Kaspa Experience Berlin 🇩🇪 on Sept 13 — join us "
               }
               <a
                 href="https://experience.kaspa.events"
